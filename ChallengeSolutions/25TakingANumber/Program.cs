a8ff1c8c2acdf79faa860f9e03a68a9f20872e96
Swizz <jddichello@gmail.com>
2024-04-16 18:17:19 -0400

    made methods for taking a number

---
+++ ChallengeSolutions/25TakingANumber/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text + " ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int AskForNumberInRange (string text, int min, int max)
+{
+    
+}
