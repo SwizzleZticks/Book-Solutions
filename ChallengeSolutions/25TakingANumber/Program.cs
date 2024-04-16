be450ca4b9a509bf60c12be638b60b59d4d43c59
Swizz <jddichello@gmail.com>
2024-04-16 19:30:42 -0400

    finish first method challenge

--- ChallengeSolutions/25TakingANumber/Program.cs
+++ ChallengeSolutions/25TakingANumber/Program.cs
@@ -1,4 +1,19 @@
-﻿
+﻿int input = AskForNumberInRange("User 1, Enter a number between 0 and 100: ", 0, 100);
+
+Console.Clear();
+
+while (true)
+{
+    int user2Input = AskForNumber("User 2, enter a number: ");
+
+    if(user2Input == input)
+    {
+        break;
+    }
+
+}
+
+Console.WriteLine("You are correct");
 
 int AskForNumber(string text)
 {
@@ -9,5 +24,13 @@ int AskForNumber(string text)
 
 int AskForNumberInRange (string text, int min, int max)
 {
-    
+   while (true)
+    {
+        int number = AskForNumber(text);
+
+        if (number >= min && number <= max)
+        {
+            return number;
+        }
+    }
 }
