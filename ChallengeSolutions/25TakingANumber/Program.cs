634f11481396e195f1853354855af4858945c94f
Swizz <jddichello@gmail.com>
2024-04-16 20:15:04 -0400

    Improvements for user guessing, and console cleanliness

--- ChallengeSolutions/25TakingANumber/Program.cs
+++ ChallengeSolutions/25TakingANumber/Program.cs
@@ -2,22 +2,38 @@
 
 Console.Clear();
 
+string? result = null;
+
 while (true)
 {
+    if (!string.IsNullOrEmpty(result))
+    {
+        Console.Clear();
+        Console.WriteLine(result);
+    }
+
     int user2Input = AskForNumber("User 2, enter a number: ");
 
     if(user2Input == input)
     {
         break;
     }
-
+    else if (user2Input > input)
+    {
+        result = "You are too high!";
+    }
+    else if(user2Input < input)
+    {
+        result= "You are too low!";
+    }
 }
 
+Console.Clear();
 Console.WriteLine("You are correct");
 
 int AskForNumber(string text)
 {
-    Console.WriteLine(text + " ");
+    Console.Write(text);
     int number = Convert.ToInt32(Console.ReadLine());
     return number;
 }
