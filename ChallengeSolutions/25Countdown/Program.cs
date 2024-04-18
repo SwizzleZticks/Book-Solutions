19b0aac2ab8f3210b2b26b9d0fcc832f20ec7a24
Swizz <jddichello@gmail.com>
2024-04-18 17:08:21 -0400

    create a method using recursion and apply a base case

---
+++ ChallengeSolutions/25Countdown/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Threading.Channels;
+
+CountDownRecursion(10);
+
+int CountDownRecursion (int Count)
+{
+    if (Count == 1)
+    {
+        Console.WriteLine("Base Case Reached.");
+        return 1;
+    }
+
+    Console.WriteLine(Count);
+    return Count * CountDownRecursion(Count - 1);
+    
+}
