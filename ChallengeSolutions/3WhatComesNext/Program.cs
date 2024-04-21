b9b5070a0fbe601a03b63ba2f28d18ed1b896ae8
Swizz <jddichello@gmail.com>
2024-04-21 14:33:06 -0400

    intitial commits with a twist to practice things learned previously

---
+++ ChallengeSolutions/3WhatComesNext/Program.cs
@@ -0,0 +1,12 @@
+﻿//Print something other than Hello World(with a twist)
+
+string forEachString = "This is my new string";
+
+char[] chars = new char[forEachString.Length];
+
+foreach(char c in forEachString)
+{
+    chars = forEachString.ToCharArray();
+    Console.WriteLine(c);
+}
+
