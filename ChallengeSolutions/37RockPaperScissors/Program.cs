e2af8caad19d0ec5d509a6f1439ae4de952d1dfe
Swizz <jddichello@gmail.com>
2024-05-04 13:25:50 -0400

    quick fix on if no selection is entered to just keep asking

--- ChallengeSolutions/37RockPaperScissors/Program.cs
+++ ChallengeSolutions/37RockPaperScissors/Program.cs
@@ -18,33 +18,35 @@ namespace _37RockPaperScissors
                 firstPlayer.GetChoice("Player 1 choose rock, paper, or scissors: ");
                 secondPlayer.GetChoice("Player 2 choose rock, paper, or scissors: ");
 
-                firstPlayer.isWinner = RPSRound.GetWinner(firstPlayer.PlayerChoice,secondPlayer.PlayerChoice);
-
-                if (firstPlayer.PlayerChoice != secondPlayer.PlayerChoice)
+                if (firstPlayer.PlayerChoice != HandChoice.None || secondPlayer.PlayerChoice != HandChoice.None)
                 {
-                    if (firstPlayer.isWinner == true)
+                    firstPlayer.isWinner = RPSRound.GetWinner(firstPlayer.PlayerChoice, secondPlayer.PlayerChoice);
+
+                    if (firstPlayer.PlayerChoice != secondPlayer.PlayerChoice)
                     {
-                        Records.Player1Score++;
-                        Console.WriteLine($"Player 1 wins with {firstPlayer.PlayerChoice}!");
-                        Console.WriteLine();
+                        if (firstPlayer.isWinner == true)
+                        {
+                            Records.Player1Score++;
+                            Console.WriteLine($"Player 1 wins with {firstPlayer.PlayerChoice}!");
+                            Console.WriteLine();
+                        }
+                        else if (firstPlayer.isWinner == false)
+                        {
+                            Records.Player2Score++;
+                            Console.WriteLine($"Players 2 wins with {secondPlayer.PlayerChoice}!");
+                            Console.WriteLine();
+                        }
                     }
-                    else if (firstPlayer.isWinner == false)
+
+                    else
                     {
-                        Records.Player2Score++;
-                        Console.WriteLine($"Players 2 wins with {secondPlayer.PlayerChoice}!");
+                        Console.WriteLine("DRAW noone wins!");
                         Console.WriteLine();
+                        Records.DrawCount++;
                     }
+
+                    Records.RoundNumber++;
                 }
-
-                else
-                {
-                    Console.WriteLine("DRAW noone wins!");
-                    Console.WriteLine();
-                    Records.DrawCount++;
-                }  
-                
-                Records.RoundNumber++;
-
             }
         }
     }
