9e10cf3345c17d7d41a9b17e7cbd7f895de21e40
Swizz <jddichello@gmail.com>
2024-05-05 14:23:55 -0400

    created logic for random number printing

---
+++ ChallengeSolutions/38FifteenPuzzle/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace _38FifteenPuzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberPool = (new int[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0}).ToList();
+
+            int[][] matrix = new int[4][];
+            matrix[0] = new int[4];
+            matrix[1] = new int[4];
+            matrix[2] = new int[4];
+            matrix[3] = new int[4];
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    matrix[x][y] = GetDeleteRandom(numberPool);
+                    Console.WriteLine(matrix[x][y]);
+                }
+            }
+        }
+        static int GetDeleteRandom(List<int> random)
+        {
+            Random r = new Random(); // optimize this so it doesn't instantiate a new random everytime
+            int accessIndex = r.Next(0,random.Count);//gets random index from an always shortening list
+            int result = random[accessIndex]; // getting the value at the random index
+            random.RemoveAt(accessIndex);  // removes value at the random index
+
+            return result;
+        }
+    }
+}
