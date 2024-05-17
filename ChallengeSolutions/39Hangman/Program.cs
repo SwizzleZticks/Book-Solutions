3b9b44988a6f9f312cd3e404bccff86bf130c78c
Joshua DiChello <jddichello@gmail.com>
2024-05-17 18:36:43 -0400

    commit

---
+++ ChallengeSolutions/39Hangman/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace _39Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int i = 0;  i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
