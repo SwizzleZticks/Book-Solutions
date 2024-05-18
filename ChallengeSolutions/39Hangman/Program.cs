109c2b21e2e679d95d6b9cac04306b5a6345b268
Joshua DiChello <jddichello@gmail.com>
2024-05-18 19:20:42 -0400

    basic logic implemented. need to add JSON file

--- ChallengeSolutions/39Hangman/Program.cs
+++ ChallengeSolutions/39Hangman/Program.cs
@@ -5,10 +5,31 @@ namespace _39Hangman
     {
         static void Main(string[] args)
         {
-            
-            for(int i = 0;  i < 10; i++)
+            char[] word = ['H', 'E', 'L', 'L', 'O'];
+            char[] replace = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
             {
-                Console.WriteLine(i);
+                replace[i] = '_';
+            }
+
+            for (int i = 0; i < word.Length; ++i)
+            {
+            
+            Console.WriteLine("Guess a letter to finish spelling the word.");
+            char? userInput = Convert.ToChar(Console.ReadLine());
+
+                if (word[i] == userInput)
+                {
+                    replace[i] = word[i];
+                }
+              
+                for (int z = 0; z < word.Length; z++)
+                {
+                    
+                    Console.Write(replace[z]);
+                }
+                Console.WriteLine();
             }
         }
     }
