6ac02ba492ac44fab4930ec6eb3181933101763e
Joshua DiChello <jddichello@gmail.com>
2024-06-12 20:34:20 -0400

    completed struct challenge

---
+++ ChallengeSolutions/45RoomCoordinates/Coordinate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45RoomCoordinates
+{
+    public readonly struct Coordinate
+    {
+        public int Row { get; init; }
+        public int Column { get; init; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+        
+        public static void AdjacentCheck(Coordinate cord, int row, int column)
+        {
+
+            if(cord.Row - row == -1 || cord.Row - row == 1)
+            {
+                Console.WriteLine("These row coordinates are adjacent");
+            }
+
+            else if(cord.Column - column == -1 || cord.Column - row == 1)
+            {
+                Console.WriteLine("These column coordinates are adjacent");
+            }
+
+            else
+            {
+                Console.WriteLine("Neither column or row are adjacent");
+            }
+        }
+    }
+}
---
+++ ChallengeSolutions/45RoomCoordinates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace _45RoomCoordinates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 0;
+            int column = 0;
+
+            while(true)
+            {
+                row = GetInt("Please enter a row coordinate: ");
+                Console.Clear();
+                column = GetInt("Please enter a column coordinate: ");
+                Console.Clear() ;
+
+                Coordinate cords = GetValue();
+                Console.Clear() ;
+                Coordinate.AdjacentCheck(cords, row, column);
+            }
+        }
+
+        static Coordinate GetValue()
+        {
+            int row = 0;
+            int column = 0;
+
+            row = GetInt("Please another enter a row coordinate: ");
+            column = GetInt("Please another enter a column coordinate: ");
+
+            return new Coordinate(row, column);
+        }
+
+        static int GetInt(string message)
+        {
+            int userInput;
+
+            while (true)
+            {
+                Console.Write(message);
+                userInput = int.Parse(Console.ReadLine());
+
+                if(userInput != 0)
+                {
+                    break;
+                }
+            }
+            return userInput;
+        }
+    }
+}
