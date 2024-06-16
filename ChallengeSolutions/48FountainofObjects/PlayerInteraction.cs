a13ce438ae9a4dc8bf7695ffe61103650d2e986b
Joshua DiChello <jddichello@gmail.com>
2024-06-15 21:49:24 -0400

    started implementing commands for interacting with objects

---
+++ ChallengeSolutions/48FountainofObjects/PlayerInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48FountainofObjects
+{
+    public interface IPlayerInteraction
+    {
+        public void Interact(FountainofObjectsGame game);
+    }
+
+    public class EnableFountain: IPlayerInteraction
+    {
+        public void Interact(FountainofObjectsGame game)
+        {
+            game.FountainState = FountainState.On;
+            Console.Write("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
+        }
+    }
+    public class ExitCave: IPlayerInteraction
+    {
+        public void Interact(FountainofObjectsGame game)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
