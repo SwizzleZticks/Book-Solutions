430e65668456de330f0aca7b31619b6ccdbd73fc
Joshua DiChello <jddichello@gmail.com>
2024-06-15 21:49:48 -0400

    added props for location of objects

--- ChallengeSolutions/48FountainofObjects/FountainofObjectsGame.cs
+++ ChallengeSolutions/48FountainofObjects/FountainofObjectsGame.cs
@@ -3,8 +3,14 @@ namespace _48FountainofObjects
 {
     public class FountainofObjectsGame
     {
+        public FountainState FountainState { get; set; } = FountainState.Off;
+        public Location Entrance {  get; set; } = new Location(0,0);
+        public Location FountainLocation { get; set; } = new Location(0, 2);
         public void Run()
         {
+            GameDisplay map = new GameDisplay();
+            Player player = new Player();
+
 
         }
     }
