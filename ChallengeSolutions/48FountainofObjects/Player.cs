5349d6680fd7c8f7de93a03f128654d91841b69a
Joshua DiChello <jddichello@gmail.com>
2024-06-15 21:48:46 -0400

    refactored player props for player movement and location assignments

--- ChallengeSolutions/48FountainofObjects/Player.cs
+++ ChallengeSolutions/48FountainofObjects/Player.cs
@@ -8,12 +8,17 @@ namespace _48FountainofObjects
 {
     public class Player
     {
-        public Location StartLocation { get; set; } = new Location(0,0);
+        public int Row { get; set; }
+        public int Column { get; set; }
         public IPlayerCommand? Command { get; set; }
 
         public void Move()
         {
             Command?.Move(this);
+        }
+        public void Interact()
+        {
+            Command?.Interact();//gonna have to use a method to get that cells state of object
         }
     }
 }
