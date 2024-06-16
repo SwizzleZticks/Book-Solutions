1e0314be81c3f3ac07992a7c6d72db16a2bdfb33
Joshua DiChello <jddichello@gmail.com>
2024-06-15 21:47:48 -0400

    implemented logic for moving player around

--- ChallengeSolutions/48FountainofObjects/PlayerCommand.cs
+++ ChallengeSolutions/48FountainofObjects/PlayerCommand.cs
@@ -10,33 +10,32 @@ namespace _48FountainofObjects
     {
         public void Move(Player player);
     }
-
     public class NorthCommand: IPlayerCommand
     {
         public void Move(Player player)
         {
-
+            if(player.Row >= 0 && player.Row < 4) player.Row++;
         }
     }
     public class EastCommand: IPlayerCommand
     {
         public void Move(Player player)
         {
-
+            if(player.Column >= 0 && player.Column < 4) player.Column++;
         }
     }
     public class SouthCommand: IPlayerCommand
     {
         public void Move(Player player)
         {
-
+            if(player.Row > 0 && player.Row <=4) player.Row--;
         }
     }
     public class WestCommand: IPlayerCommand
     {
         public void Move(Player player)
         {
-
+            if (player.Column > 0 & player.Column <= 4) player.Column--;
         }
     }
 }
