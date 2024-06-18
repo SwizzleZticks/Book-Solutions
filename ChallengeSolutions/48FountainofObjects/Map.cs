f4db7af461e32805a6d2ac7ded5af6d7b365c646
Joshua DiChello <jddichello@gmail.com>
2024-06-17 22:05:39 -0400

    added helper methods, and check to see if move is on map

--- ChallengeSolutions/48FountainofObjects/Map.cs
+++ ChallengeSolutions/48FountainofObjects/Map.cs
@@ -13,5 +13,30 @@ namespace _48FountainofObjects
             _rooms = new RoomType[row, column];
         }
 
+        public RoomType GetRoomTypeAtLocation(Location location)
+        {
+            return _rooms[location.Row, location.Column];
+        }
+
+        public bool IsOnMap(Location location)
+        {
+            if((location.Row >= 0 && location.Row < _rooms.Length) && (location.Column >= 0 && location.Column <= _rooms.Length ))
+            {
+                return true;
+            }
+            return false;
+        }
+        public RoomType SetRoomType(Location location, RoomType roomType)
+        {
+            return _rooms[location.Row,location.Column];
+        }
+
+    }
+    public enum RoomType
+    {
+        Normal,
+        Entrance,
+        Fountain,
+        OffTheMap
     }
 }
