95cf311407731c4efd6fc91f14fdf4c3a5833850
Joshua DiChello <jddichello@gmail.com>
2024-06-17 22:06:05 -0400

    refactored player class

--- ChallengeSolutions/48FountainofObjects/Player.cs
+++ ChallengeSolutions/48FountainofObjects/Player.cs
@@ -3,17 +3,12 @@ namespace _48FountainofObjects
 {
     public class Player
     {
-        public int Row { get; set; }
-        public int Column { get; set; }
-        public ICommand? Command { get; set; }
+        public Location Location { get; set; }
+        public bool IsAlive { get; set; } = true;
+        public Player(Location start)
+        {
+            Location = start;
+        }
 
-        public void Move()
-        {
-            Command?.ICommand(this);
-        }
-        public void Interact()
-        {
-            Command?.Interact();//gonna have to use a method to get that cells state of object
-        }
     }
 }
