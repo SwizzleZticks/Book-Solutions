096a9cbf9cccafe079e156c199128f820d791d0d
Joshua DiChello <jddichello@gmail.com>
2024-06-21 19:37:50 -0400

    added props and instantiated monsters, added senses for rooms and fixed bug not displaying room status, completed logic

--- ChallengeSolutions/48FountainofObjects/FountainOfObjectsGame.cs
+++ ChallengeSolutions/48FountainofObjects/FountainOfObjectsGame.cs
@@ -2,36 +2,78 @@
 
 namespace _48FountainofObjects
 {
+    
     public record Location(int Row, int Column);
     public class FountainOfObjectsGame
     {
         public Player Player { get; }
         public Map Map { get; }
-        public bool IsFountainOn { get; set; }
-        public FountainOfObjectsGame(Player player,Map map)
+        public bool IsFountainOn { get; set; } = false;
+        public Monster[] Monsters { get; }
+
+        private readonly ISense[] _senses;
+        public FountainOfObjectsGame(Player player, Map map, Monster[] monsters)
         {
             Player = player;
             Map = map;
-        }
-        FountainOfObjectsGame CreateGame()
-        {
-            Map map = new Map(4, 4);
-            Location start = new Location(0, 0);
-            map.SetRoomType(start, RoomType.Entrance);
-            map.SetRoomType(new Location(0, 2), RoomType.Fountain);
+            Monsters = monsters;
 
-            return new FountainOfObjectsGame(new Player(start), map);
+            _senses = new ISense[]
+            {
+                new LightInEntranceSense(),
+                new FountainSense()
+            };
         }
         public void Run()
         {
-            GameStatus();
-        }
-        private void GameStatus()
-        {
-            Console.WriteLine("-------------------------------------------------------------------------------------------------");
-            Console.Write($"You are in the room at (Row={ Player.Location.Row }, Column={ Player.Location.Column }).");
-        }
+            while (!HasWon && Player.IsAlive)
+            {
+                DisplayStatus();
+                ICommand command = GetCommand();               
+                command.Execute(this);
 
+                foreach (Monster monster in Monsters)
+                    if (monster.Location == Player.Location && monster.IsAlive) monster.Activate(this);
+            }
+
+            if (HasWon)
+            {
+                ConsoleHelper.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!", ConsoleColor.DarkGreen);
+                ConsoleHelper.WriteLine("You win!", ConsoleColor.DarkGreen);
+            }
+            else
+            {
+                ConsoleHelper.WriteLine(Player.CauseOfDeath, ConsoleColor.Red);
+                ConsoleHelper.WriteLine("You lost.", ConsoleColor.Red);
+            }
+        }
+        private void DisplayStatus()
+        {
+            ConsoleHelper.WriteLine("--------------------------------------------------------------------------------", ConsoleColor.DarkGreen);
+            ConsoleHelper.WriteLine($"You are in the room at (Row={ Player.Location.Row }, Column={ Player.Location.Column }).", ConsoleColor.Gray);
+            foreach (ISense sense in _senses)
+            {
+                if (sense.CanSense(this))
+                    sense.DisplaySense(this);
+            }
+        }
+        private ICommand GetCommand()
+        {
+            while (true)
+            {
+                ConsoleHelper.Write("What do you want to do?", ConsoleColor.White);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                string? input = Console.ReadLine();
+
+                if (input == "move north") return new MoveCommand(Direction.North);
+                if (input == "move south") return new MoveCommand(Direction.South);
+                if (input == "move east") return new MoveCommand(Direction.East);
+                if (input == "move west") return new MoveCommand(Direction.West);
+                if (input == "enable fountain") return new EnableFountain();
+
+                ConsoleHelper.WriteLine($"I did not understand '{input}'.", ConsoleColor.Red);
+            }
+        }
         public RoomType CurrentRoom => Map.GetRoomTypeAtLocation(Player.Location);
 
         public bool HasWon => CurrentRoom == RoomType.Entrance && IsFountainOn;
