2b6d60eb434f4ddc7f191bfd0125d07780a2ef3e
Joshua DiChello <jddichello@gmail.com>
2024-06-21 19:39:35 -0400

    added helper class for text color

---
+++ ChallengeSolutions/48FountainofObjects/ConsoleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48FountainofObjects
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteLine(string text, ConsoleColor color) 
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+        }
+        public static void Write(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+        }
+    }
+}
