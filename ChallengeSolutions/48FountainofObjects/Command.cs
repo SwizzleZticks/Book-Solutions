dbd3bb84f83c2be1dbf3dd178f75c23366338a60
Joshua DiChello <jddichello@gmail.com>
2024-06-21 19:36:55 -0400

    move lines around to make it easier to read

--- ChallengeSolutions/48FountainofObjects/Command.cs
+++ ChallengeSolutions/48FountainofObjects/Command.cs
@@ -25,34 +25,31 @@ namespace _48FountainofObjects
             Location currentLocation = game.Player.Location;
             Location newLocation = Direction switch
             {
-                Direction.North => new Location(currentLocation.Row + 1, currentLocation.Column),
-                Direction.East => new Location(currentLocation.Row, currentLocation.Column + 1),
-                Direction.South => new Location(currentLocation.Row - 1, currentLocation.Column),
-                Direction.West => new Location(currentLocation.Row, currentLocation.Column - 1)
+                Direction.North => new Location(currentLocation.Row - 1, currentLocation.Column),
+                Direction.South => new Location(currentLocation.Row + 1, currentLocation.Column),
+                Direction.West => new Location(currentLocation.Row, currentLocation.Column - 1),
+                Direction.East => new Location(currentLocation.Row, currentLocation.Column + 1)
             };
-            if (game.Map.IsOnMap(newLocation))
-            {
-                game.Player.Location = newLocation;
-            }
-            else
-            {
-                Console.WriteLine("There is a wall there");
-            }
 
+            if (game.Map.IsOnMap(newLocation))
+                game.Player.Location = newLocation;
+            else
+                ConsoleHelper.WriteLine("There is a wall there.", ConsoleColor.Red);
         }
     }
     public class EnableFountain : ICommand
     {
         public void Execute(FountainOfObjectsGame game)
         {
-
+            if (game.Map.GetRoomTypeAtLocation(game.Player.Location) == RoomType.Fountain) game.IsFountainOn = true;
+            else ConsoleHelper.WriteLine("The fountain is not in this room. There was no effect.", ConsoleColor.Red);
         }
     }
 }
 public enum Direction
 {
     North,
-    East,
     South,
-    West
+    West,
+    East
 }
