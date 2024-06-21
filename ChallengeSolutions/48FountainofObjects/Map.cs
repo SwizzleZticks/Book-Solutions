88703856be482fd1798005dd734bae5bc50dff55
Joshua DiChello <jddichello@gmail.com>
2024-06-21 19:38:18 -0400

    renamed props to fit the game grid

--- ChallengeSolutions/48FountainofObjects/Map.cs
+++ ChallengeSolutions/48FountainofObjects/Map.cs
@@ -4,28 +4,36 @@ namespace _48FountainofObjects
     public class Map
     {
         private readonly RoomType[,] _rooms;
-        public int Row { get; set; }
-        public int Column { get; set; }
-        public Map(int row, int column)
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public Map(int rows, int columns)
         {
-            Row = row;
-            Column = column;
-            _rooms = new RoomType[row, column];
+            Rows = rows;
+            Columns = columns;
+            _rooms = new RoomType[rows, columns];
         }
-
-        public RoomType GetRoomTypeAtLocation(Location location)
+        public bool HasNeighborWithType(Location location, RoomType roomType)
         {
-            return _rooms[location.Row, location.Column];
-        }
-
-        public bool IsOnMap(Location location)
-        {
-            if((location.Row >= 0 && location.Row < _rooms.Length) && (location.Column >= 0 && location.Column <= _rooms.Length ))
-            {
-                return true;
-            }
+            if (GetRoomTypeAtLocation(new Location(location.Row - 1, location.Column - 1)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row - 1, location.Column)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row - 1, location.Column + 1)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row, location.Column - 1)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row, location.Column)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row, location.Column + 1)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row + 1, location.Column - 1)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row + 1, location.Column)) == roomType) return true;
+            if (GetRoomTypeAtLocation(new Location(location.Row + 1, location.Column + 1)) == roomType) return true;
             return false;
         }
+
+        public RoomType GetRoomTypeAtLocation(Location location) => IsOnMap(location) ? _rooms[location.Row, location.Column] : RoomType.OffTheMap;
+        public void SetRoomTypeAtLocation(Location location, RoomType type) => _rooms[location.Row, location.Column] = type;
+
+        public bool IsOnMap(Location location) =>
+            location.Row >= 0 &&
+            location.Row < _rooms.GetLength(0) &&
+            location.Column >= 0 &&
+            location.Column < _rooms.GetLength(1);
         public RoomType SetRoomType(Location location, RoomType roomType)
         {
             return _rooms[location.Row,location.Column];
