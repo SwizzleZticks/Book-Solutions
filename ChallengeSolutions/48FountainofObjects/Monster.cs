04efc2c31d75d78406a5a612cb359aba99fd20bf
Joshua DiChello <jddichello@gmail.com>
2024-06-21 19:39:50 -0400

    added for future use to expand game

---
+++ ChallengeSolutions/48FountainofObjects/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48FountainofObjects
+{
+    public abstract class Monster
+    {
+        public Location Location { get; set; }
+
+        public bool IsAlive { get; set; } = true;
+
+        public Monster(Location start) => Location = start;
+        public abstract void Activate(FountainOfObjectsGame game);
+    }
+}
