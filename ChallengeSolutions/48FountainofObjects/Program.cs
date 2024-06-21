827cafd3c89020e15596464b0e393740e68ddab3
Joshua DiChello <jddichello@gmail.com>
2024-06-21 19:39:07 -0400

    added the finishing touches to the creating game initializer

--- ChallengeSolutions/48FountainofObjects/Program.cs
+++ ChallengeSolutions/48FountainofObjects/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using _48FountainofObjects;
+
+FountainOfObjectsGame game = CreateSmallGame();
+game.Run();
+
+FountainOfObjectsGame CreateSmallGame()
+{
+    Map map = new Map(4, 4);
+    Location start = new Location(0, 0);
+    map.SetRoomTypeAtLocation(start, RoomType.Entrance);
+    map.SetRoomTypeAtLocation(new Location(0, 2), RoomType.Fountain);
+
+    Monster[] monsters = new Monster[] { };
+
+    return new FountainOfObjectsGame(new Player(start), map, monsters);
+}
