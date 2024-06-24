a5d4481ce9f86ff3de6ebd96c5b91cddd466acba
Joshua DiChello <jddichello@gmail.com>
2024-06-24 16:52:37 -0400

    completed challenge using lists

---
+++ ChallengeSolutions/51ListofCommands/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace _51ListofCommands
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot();
+
+            string? input = null;
+
+            while (input != "stop") 
+            {
+                Console.WriteLine("Please enter a command(on,off,north,south,west,east, or stop to exit:");
+                input = Console.ReadLine().ToLower();
+
+                switch (input)
+                {
+                    case "on":
+                        new OnCommand();
+                        break;
+                    case "off":
+                        new OffCommand();
+                        break;
+                    case "north":
+                        new NorthCommand();
+                        break;
+                    case "south":
+                        new SouthCommand();
+                        break;
+                    case "west":
+                        new WestCommand();
+                        break;
+                    case "east":
+                        new EastCommand();
+                        break;
+                    case "stop":
+                        break;                      
+                };
+            }
+
+            Console.WriteLine();
+            robot.Run();
+        }
+    }
+}
---
+++ ChallengeSolutions/51ListofCommands/Robot.cs
@@ -0,0 +1,19 @@
+﻿
+namespace _51ListofCommands
+{
+    public class Robot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsPowered { get; set; }
+        public List<IRobotCommand> Commands { get; } = new List<IRobotCommand>();
+        public void Run()
+        {
+            foreach (IRobotCommand? command in Commands)
+            {
+                command?.Run(this);
+                Console.WriteLine($"[{X} {Y} {IsPowered}]");
+            }
+        }
+    }
+}
