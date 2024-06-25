121575ebae055de6d0c842f5afeb7cf01f13e2be
Joshua DiChello <jddichello@gmail.com>
2024-06-25 16:40:32 -0400

    completed and combined 52 and 53 challenges into one

---
+++ ChallengeSolutions/52TheFued/Program.cs
@@ -0,0 +1,18 @@
+﻿using IField;
+using McDroid;
+
+namespace _52TheFued
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IField.Pig pig = new IField.Pig();
+            Sheep sheep = new Sheep();
+
+            Cow cow = new Cow();
+            McDroid.Pig pig1 = new McDroid.Pig();
+        }
+    }
+}
