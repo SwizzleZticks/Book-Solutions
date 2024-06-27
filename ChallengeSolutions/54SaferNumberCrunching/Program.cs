defb5ab5ff417bd7a1ae59a5b8e91e74227124fa
Joshua DiChello <jddichello@gmail.com>
2024-06-27 12:24:45 -0400

    completed methods revisited challenge

---
+++ ChallengeSolutions/54SaferNumberCrunching/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int number;
+do
+{
+    Console.WriteLine("Enter a integer: ");
+}
+while (!int.TryParse(Console.ReadLine(), out number));
+
+double number2;
+do
+{
+    Console.WriteLine("Enter a double: ");
+}
+while (!double.TryParse(Console.ReadLine(),out number2));
+
+
+bool truthValue;
+do
+{
+    Console.WriteLine("Enter a boolean");
+}
+while(!bool.TryParse(Console.ReadLine(),out  truthValue));
