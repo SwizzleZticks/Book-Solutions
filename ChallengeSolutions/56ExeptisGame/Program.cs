cd2c24e68135170e1a62930dea461f2c53ac9d0f
Joshua DiChello <jddichello@gmail.com>
2024-07-06 16:22:31 -0400

    completed challenge, awaiting review to make corrections

---
+++ ChallengeSolutions/56ExeptisGame/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _56ExeptisGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> usedNumbers = new List<int>();
+
+            while (true)
+            {
+                int randomNumber = random.Next(0,10);
+
+                usedNumbers.Add(randomNumber);
+
+                try
+                {
+                    Console.Write("Enter a number between 0 and 9: ");
+                    int userInput = Convert.ToInt32(Console.ReadLine());
+
+                    if (usedNumbers.Contains(userInput)) throw new Exception("This number has already been used");
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex);                   
+                }
+            }           
+        }
+    }
+}
