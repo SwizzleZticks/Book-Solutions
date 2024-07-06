3d9a1be490224c028abdc1f7c2784ff5c0ba8514
Joshua DiChello <jddichello@gmail.com>
2024-07-06 17:02:40 -0400

    completed the challenge with refactoring

--- ChallengeSolutions/56ExeptisGame/Program.cs
+++ ChallengeSolutions/56ExeptisGame/Program.cs
@@ -4,25 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            List<int> usedNumbers = new List<int>();
-
-            while (true)
+            try
             {
-                try
-                {
-                    Console.Write("Enter a number between 0 and 9: ");
-                    int userInput = Convert.ToInt32(Console.ReadLine());
+                int targetNumber = new Random().Next(10);
+                List<int> usedNumbers = new List<int>();
 
-                    if (usedNumbers.Contains(userInput)) throw new Exception("This number has already been used");
-                }
-                catch (Exception ex) 
+                while (true)
                 {
-                    Console.WriteLine(ex);                   
-                }
+                    bool previouslyGuessed;
+                    int userInput;
 
-                usedNumbers.Add(random.Next(0,10));
-            }           
+                    do
+                    {
+                        Console.Write("Enter a number between 0 and 9: ");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                        previouslyGuessed = usedNumbers.Contains(targetNumber);
+
+
+                        if (previouslyGuessed) Console.WriteLine("That number has been guessed before");
+                    } while (previouslyGuessed);
+
+
+                    if (userInput == targetNumber) throw new Exception();
+
+                    usedNumbers.Add(userInput);
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("bad number, you lose.");
+            }
         }
     }
 }
