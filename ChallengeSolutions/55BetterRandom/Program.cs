4282ab526e8d790fb916dba52ceea28f41c44e2f
Joshua DiChello <jddichello@gmail.com>
2024-07-06 12:18:59 -0400

    completed challenge on methods

---
+++ ChallengeSolutions/55BetterRandom/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _55BetterRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine(rnd.NextDouble(100));
+            Console.WriteLine(rnd.NextString("this","is","a","test"));
+            Console.WriteLine(rnd.CoinFlip(0.25));
+
+        }
+    }
+}
---
+++ ChallengeSolutions/55BetterRandom/RandomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55BetterRandom
+{
+    public static class RandomExtensions
+    {
+        public static double NextDouble(this Random random, double max) => random.NextDouble() * max;
+        public static string NextString(this Random random, params string[] options) => options[random.Next(options.Length)];
+        public static bool CoinFlip(this Random random, double headFreq = 0.5) => random.Next() < headFreq;
+    }
+}
