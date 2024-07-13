bfa23c11ceb7361ceff8a8714840789b23e0575e
Joshua DiChello <jddichello@gmail.com>
2024-07-13 17:11:57 -0400

    other commit didnt push, repush.

--- ChallengeSolutions/51ListofCommands/Program.cs
+++ ChallengeSolutions/51ListofCommands/Program.cs
@@ -7,39 +7,25 @@ namespace _51ListofCommands
         {
             Robot robot = new Robot();
 
-            string? input = null;
-
-            while (input != "stop") 
+            while (true)
             {
-                Console.WriteLine("Please enter a command(on,off,north,south,west,east, or stop to exit:");
-                input = Console.ReadLine().ToLower();
+                string? input = Console.ReadLine();
 
-                switch (input)
+                if (input == "stop") break;
+
+                robot.Commands.Add(input switch
                 {
-                    case "on":
-                        robot.Commands.Add(new OnCommand());
-                        break;
-                    case "off":
-                        robot.Commands.Add(new OffCommand());
-                        break;
-                    case "north":
-                        robot.Commands.Add(new NorthCommand());
-                        break;
-                    case "south":
-                        robot.Commands.Add(new SouthCommand());
-                        break;
-                    case "west":
-                        robot.Commands.Add(new WestCommand());
-                        break;
-                    case "east":
-                        robot.Commands.Add(new EastCommand());
-                        break;
-                    case "stop":
-                        break;                      
-                };
+                    "on" => new OnCommand(),
+                    "off" => new OffCommand(),
+                    "north" => new NorthCommand(),
+                    "south" => new SouthCommand(),
+                    "east" => new EastCommand(),
+                    "west" => new WestCommand(),
+                });
             }
 
             Console.WriteLine();
+
             robot.Run();
         }
     }
