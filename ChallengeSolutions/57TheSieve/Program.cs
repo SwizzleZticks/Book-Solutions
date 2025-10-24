bda0e6e5952bca5940ee1f78ef2c90460f80a018
SwizzleZticks <jddichello@gmail.com>
2025-10-24 07:25:45 -0400

    complete challenge on delegates

---
+++ ChallengeSolutions/57TheSieve/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _57TheSieve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a filter:");
+            Console.WriteLine("1: Check even number");
+            Console.WriteLine("2: Check is positive number");
+            Console.WriteLine("3: Check is multiple of 10");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Sieve sieve = userInput switch
+            {
+                1 => new Sieve(IsEven),
+                2 => new Sieve(IsPositive),
+                3 => new Sieve(IsMultipleOfTen)
+
+            };
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Filter selection result {sieve.IsGood(number)}.");
+            }
+        }
+        static bool IsEven(int number) => number % 2 == 0;
+        static bool IsPositive(int number) => number > 0;
+        static bool IsMultipleOfTen(int number) => number % 10 == 0;
+
+    }
+}
---
+++ ChallengeSolutions/57TheSieve/Sieve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57TheSieve
+{
+    public class Sieve
+    {
+        private Func<int, bool> _filter;
+
+        public Sieve(Func<int, bool> filter)
+        {
+            _filter = filter;
+        }
+        public bool IsGood(int number) 
+        {
+            return _filter(number);
+        }
+    }
+}
